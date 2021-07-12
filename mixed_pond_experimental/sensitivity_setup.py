924aec6de54d208f1b167d3dfb44207d35a32332
Sanketa Kadam <kadamsanketa18@gmail.com>
2021-07-11 23:46:10 -0400

    trying to debug the changed equations for heat

--- mixed_pond_experimental/sensitivity_setup.py
+++ mixed_pond_experimental/sensitivity_setup.py
@@ -11,6 +11,8 @@ import pandas as pd
 import math
 import matplotlib.pyplot as plt
 
+
+
 #Setting constant values 
 
 pi = 3.1415 
@@ -25,7 +27,9 @@ srad_names = ['SRAD00', 'SRAD03', 'SRAD06', 'SRAD09','SRAD12', 'SRAD15', 'SRAD18
 W_z = 1.733 #m/s
 SRAD = 15.4618
 Rh = 76.211
-
+volume = 6153.05 #m3
+area = 4047 #m2
+t = 24 #hrs
 
 #open csv file using pandas to create pandas dataframe 
 
@@ -36,7 +40,7 @@ air_temp_data = pd.read_csv("diurnal_air_temp_khulna_daily.csv") #this value is 
 # file is set up)
 #data.day[i] needs= float for timedelta() func; (data.day[i] = int in pandas df) 
 def find_day_month_year(input_day, start_day = dt.date(2016,12,31)):
-    computed_day = start_day + dt.timedelta(days = float(input_day))
+    computed_day = start_day + dt.timedelta(days = input_day)
     days_from_year_start = computed_day - dt.date(computed_day.year, 1, 1) + dt.timedelta(days = 1)
 
     return days_from_year_start.days, computed_day
@@ -46,6 +50,7 @@ def find_day_month_year(input_day, start_day = dt.date(2016,12,31)):
 #calculate phi_sn pr penetrating short-wae solar radiation
 
 def calculate_phi_sn(day_argue):
+    print(day_argue)
    
     R_s = 0.035 #considering constant value for daily code #Losordo&Piedrahita
 
@@ -54,7 +59,7 @@ def calculate_phi_sn(day_argue):
     
     phi_s = SRAD  #Kj/m2/hr
 
-    phi_sn = float(phi_s * (1-R))
+    phi_sn = phi_s * (1-R)
     
     return phi_sn
 
@@ -73,11 +78,13 @@ def read_air_temp(day_argue):
 
 
 def calculate_phi_at(day_argue):
+    print(day_argue)
     air_temp_line = read_air_temp(day_argue)
-    T_ak = air_temp_line['avg_temp'] #in kelvin
+    T_ak = float(air_temp_line['avg_temp']) #in kelvin
+    print(T_ak)
     e = (0.398 * (10 ** (-5)))*(T_ak ** (2.148))
     r = 0.03 # reflectance of the water surface to longwave radiation
-    phi_at = float((1-r)*e*sigma*((T_ak)**4))
+    phi_at = (1-r)*e*sigma*((T_ak)**4)
 
     return(phi_at)
 
@@ -86,7 +93,7 @@ def calculate_phi_at(day_argue):
 # from the water temp data file (ideally Jan 1st 2018, morning water temp)
 
 def calculate_phi_ws(T_wk, day_argue):
-    
+    print(T_wk)
 
 # set T_wk such that it reads the final output water temp from results in a loop
     
@@ -98,24 +105,26 @@ def calculate_phi_ws(T_wk, day_argue):
 #here, we use average dailt dew point temp in place of relative humidity values
 
 def calculate_phi_e(T_wk,day_argue):
+    print(T_wk)
 
     W_2 = W_z * 3.6
     air_temp_line = read_air_temp(day_argue)
-    T_ak = air_temp_line['avg_temp'] #kelvin
+    T_ak = float(air_temp_line['avg_temp']) #kelvin
+    print(T_ak)
     T_ac = T_ak -273.15 #degree celcius
 
-# e_s, saturated vapor pressure needs to be in T_wc deg celcius
+    # e_s, saturated vapor pressure needs to be in T_wc deg celcius
 
     T_wc = T_wk - 273.15
     e_s = 25.374 * math.exp(17.62 - 5271/T_wc)
 
     RH = Rh
 
-# e_a, water vapor pressure above the pond surface; unit mmHg
+    # e_a, water vapor pressure above the pond surface; unit mmHg
     
     e_a = RH * 25.374 * math.exp(17.62 - 5271/T_ac)     
 
-    phi_e = float(N* W_2 * (e_s- e_a))
+    phi_e = N* W_2 * (e_s- e_a)
     return(phi_e)
 
 #create function for phi_c sensible heat transfer
@@ -123,53 +132,65 @@ def calculate_phi_e(T_wk,day_argue):
 def calculate_phi_c(T_wk, day_argue):
    
     air_temp_line = read_air_temp(day_argue)
-    T_ak = air_temp_line['avg_temp'] #kelvin
-    
+    print(air_temp_line)
+    T_ak = float(air_temp_line['avg_temp']) #kelvin
+    print(T_ak)
     T_wc = T_wk - 273.15 #convert to deg celcius
     T_ac = T_ak - 273.15 
 
-    phi_c = float(1.5701 * W_z * (T_wc-T_ac))
+    phi_c = 1.5701 * W_z * (T_wc-T_ac)
 
     return(phi_c)
 
 ############### Creating simulation loop for daily values #############################
 # Energy Flux equation: phi_net = phi_sn + phi_at - phi_ws - phi_e - phi_c   
-# Heat at t-1 time step: H_t_1 = T_wk * water_heat_capacity * water_density
-# Heat at t: H_t = H_t_1 + phi_net
-# T_w = H_t/ (water_heat_capacity * water_density)
+# Heat at t-1 time step: H_t_1 = T_wk * volume * water_heat_capacity * water_density
+# Heat at t: H_t = H_t_1 + (phi_net * area * t)
+# T_w = T_wc + H_t/ (volume * water_heat_capacity * water_density)
 
 # loop for energy flux equation 
 def main_simulation_loop():
-
+    
     global T_wk
     count = 0
     for day_argue in list(range(1, 1096)):
+        
         
         count = count + 1
 
         print(f"Iteration {count} start - T_wk: {T_wk}")
 
         phi_sn = calculate_phi_sn(day_argue)
+        print(f'phi_sn: {phi_sn}')
         phi_at = calculate_phi_at(day_argue)
+        print(f'phi_at: {phi_at}')
         phi_ws = calculate_phi_ws(T_wk, day_argue)
+        print(f'phi_ws: {phi_ws}')  
         phi_e = calculate_phi_e(T_wk, day_argue)
+        print(f'phi_e: {phi_e}')
         phi_c = calculate_phi_c(T_wk, day_argue)
-            
+        print(f'phi_c: {phi_c}')
+        
         phi_net = phi_sn + phi_at - phi_ws - phi_e - phi_c 
 
-        print(phi_net)
+        print(f'iteration: {count}, phi_net: {phi_net}')
+        
+        T_wC = T_wk - 273.15 #change to degree celcius   
 
-        T_wC = T_wk - 273.15 #change to degree celcius
 
-        H_t_1 = T_wC * water_heat_capacity * water_density
+        H_t_1 = T_wC * volume * water_heat_capacity * water_density
+        #check if K or C
+        print(f'iteration: {count}, H_t_1: {H_t_1}')
 
-        H_t = H_t_1 + phi_net
-        T_w = H_t/ (water_heat_capacity * water_density)
+        H_t = H_t_1 + (phi_net * area * t)
+        T_w = T_wC + H_t/ (volume * water_heat_capacity * water_density)
+        print(f'iteration: {count}, T_w: {T_w}')
 
         #add T_w to a list somehow
         T_wC_vec.append(T_w)
 
         T_wk = T_w + 273.15 #convert back to kelvin
+        print(T_wk)
 
 
     print(T_wC_vec)
@@ -189,5 +210,4 @@ def main_simulation_loop():
 if __name__ == '__main__':
     main_simulation_loop()
     
-
 
