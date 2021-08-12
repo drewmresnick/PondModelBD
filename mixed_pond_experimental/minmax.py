f3f792820ec4065b5704ff4f9ef58f574f02efd3
Sanketa Kadam <kadamsanketa18@gmail.com>
2021-08-12 11:59:43 -0400

    changes to min max file per discussions

--- mixed_pond_experimental/minmax.py
+++ mixed_pond_experimental/minmax.py
@@ -26,10 +26,10 @@ srad_names = ['SRAD00', 'SRAD03', 'SRAD06', 'SRAD09','SRAD12', 'SRAD15', 'SRAD18
 volume = 6153.05 #m3
 area = 4047 #m2
 t = 24 #hrs
-
+T_wc_0 = 19.3
 
 #open csv file using pandas to create pandas dataframe 
-data = pd.read_csv("input_data_for_simulation_2017_2019_khulna.csv") 
+data = pd.read_csv("new_data.csv") 
 relative_humidity = pd.read_csv("khulna_relativeHumidity_2m.csv") 
 observedtemp = pd.read_csv('Air_water_temp_dataset_Sylhet_Khulna_modified.csv')
 
@@ -50,7 +50,7 @@ def read_dataline(day_argue):
     day, day_mon_year = find_day_month_year(day_argue)
     year = day_mon_year.year
     
-    selected_data = data[(data['day']== day) & (data['year'] == year)]
+    selected_data = data[(data['DOY']== day) & (data['YEAR'] == year)]
             
     return selected_data      
              
@@ -62,17 +62,18 @@ def read_dataline(day_argue):
 
 def calculate_phi_sn(day_argue):
     daily_data = read_dataline(day_argue)
-    wind_speed = float(daily_data['wind_speed_2m'])
+    wind_speed = float(daily_data['WS2M'])
     
     R_s = 0.035 #considering constant value for daily code #Losordo&Piedrahita
 
-    W_z = wind_speed #wind velocity in m/s
+    W_z = wind_speed  #wind velocity in m/s
     
     R= R_s *(1-0.08 * W_z)
     
-    phi_s = float(daily_data['SRAD_24'])  #Kj/m2/hr
+    phi_s = float(daily_data['SRAD_kj'])  #Kj/day
 
     phi_sn = phi_s * (1-R)
+    
     
     return phi_sn
 
@@ -83,11 +84,14 @@ def calculate_phi_sn(day_argue):
 
 
 def calculate_phi_at(day_argue):
-    T_ak = observedtemp[(observedtemp['day']== day_argue)]
-    T_ak = float(T_ak['min_air_temp_khu']) +273.15
+    daily_data = read_dataline(day_argue)
+    
+    T_ak = float(daily_data['air_min']) +273.15
     e = (0.398 * (10 ** (-5)))*(T_ak ** (2.148))
+
     r = 0.035 # reflectance of the water surface to longwave radiation
     phi_at = (1-r)*e*sigma*((T_ak)**4)
+    
 
     return(phi_at)
 
@@ -101,6 +105,8 @@ def calculate_phi_ws(T_wk, day_argue):
 # set T_wk such that it reads the final output water temp from results in a loop
     
     phi_ws = 0.97 * sigma * ((T_wk)**4)
+    
+
 
     return(phi_ws)
 
@@ -109,29 +115,30 @@ def calculate_phi_ws(T_wk, day_argue):
 
 def calculate_phi_e(T_wk,day_argue):
     daily_data = read_dataline(day_argue)
-    wind_speed = float(daily_data['wind_speed_2m'])
+    wind_speed = float(daily_data['WS2M'])
 
-    W_2 = wind_speed * 3.6
-    
-    T_ak = observedtemp[(observedtemp['day']== day_argue)]
-    T_ac = float(T_ak['min_air_temp_khu']) 
-    T_ak = T_ac +273.15
+    W_2 = wind_speed 
     
 
+    T_ak = float(daily_data['air_min']) +273.15
+
+    
 # e_s, saturated vapor pressure needs to be in T_wc deg celcius
 
     
     e_s = 25.374 * math.exp(17.62 - 5271/T_wk)
     
-    RH = relative_humidity[(relative_humidity['day']== day_argue)]
+    RH = float(daily_data['RH2M']) 
 
-    RH = float(RH['RH2M'])
 
 # e_a, water vapor pressure above the pond surface; unit mmHg
     
     e_a = RH * 25.374 * math.exp(17.62 - 5271/T_ak)     
 
     phi_e = float(N* W_2 * (e_s- e_a))
+    
+    
+    
     return(phi_e)
 
 #create function for phi_c sensible heat transfer
@@ -140,14 +147,15 @@ def calculate_phi_c(T_wk, day_argue):
     daily_data = read_dataline(day_argue)
     wind_speed = daily_data['WS2M']
 
-    W = float(wind_speed) #m/s per C&B paper
+    W = float(wind_speed)  #m/s per C&B paper
     
-    T_ak = observedtemp[(observedtemp['day']== day_argue)]
-    T_ac = float(T_ak['min_air_temp_khu']) 
+    T_ac = float(daily_data['air_min'])
     
     T_wc = T_wk - 273.15 #convert to deg celcius
     
     phi_c = float(1.5701 * W * (T_wc-T_ac))
+    
+ 
 
     return(phi_c)
 
@@ -182,19 +190,20 @@ def main_simulation_loop():
         
         phi_net = phi_sn + phi_at - phi_ws - phi_e - phi_c 
         
-        
+       
 
         print(f'iteration: {count}, phi_net: {phi_net}')
         
         T_wC = T_wk - 273.15 #change to degree celcius   
 
 
-        H_t_1 = T_wC * volume * water_heat_capacity * water_density
+        #H_t_1 = T_wC * volume * water_heat_capacity * water_density
         #check if K or C
-        print(f'iteration: {count}, H_t_1: {H_t_1}')
+        #print(f'iteration: {count}, H_t_1: {H_t_1}')
 
-        H_t = H_t_1 + (phi_net * area * t)
-        T_w = H_t/ (volume * water_heat_capacity * water_density)
+        #H_t = H_t_1 + (phi_net * area)
+        T_w = T_wc_0 + (((T_wC * volume * water_heat_capacity * water_density)+(phi_net * area))/ (volume * water_heat_capacity * water_density))
+                    
         print(f'iteration: {count}, T_w: {T_w}')
 
         #add T_w to a list somehow
@@ -240,26 +249,30 @@ def calculate_phi_sn(day_argue):
     
     R= R_s *(1-0.08 * W_z)
     
-    phi_s = float(daily_data['SRAD_24'])  #Kj/m2/hr
+    phi_s = float(daily_data['SRAD_kj'])  #Kj/m2/hr
 
     phi_sn = phi_s * (1-R)
+    
+    phi_sn = phi_sn*24 #convert to day
     
     return phi_sn
 
 #create function to open air_temp file for day month and year
-
-
 
 #creating a function for phi_at atmospheric radiation following the equation in variables excel sheet
 #shammun includes cloud fraction in the equation
 
 
 def calculate_phi_at(day_argue):
-    T_ak = observedtemp[(observedtemp['day']== day_argue)]
-    T_ak = float(T_ak['max_air_temp_khu']) + 273.15
-    e = (0.398 * (10 ** (-5)))*(T_ak ** (2.148))
-    r = 0.03 # reflectance of the water surface to longwave radiation
+    daily_data = read_dataline(day_argue)
+    
+    T_ak = float(daily_data['air_max']) +273.15
+    #e = (0.398 * (10 ** (-5)))*(T_ak ** (2.148))
+    e = 0.97
+    r = 0.035 # reflectance of the water surface to longwave radiation
     phi_at = (1-r)*e*sigma*((T_ak)**4)
+    
+    phi_at = phi_at *24
 
     return(phi_at)
 
@@ -273,6 +286,8 @@ def calculate_phi_ws(T_wk, day_argue):
 # set T_wk such that it reads the final output water temp from results in a loop
     
     phi_ws = 0.97 * sigma * ((T_wk)**4)
+    
+    phi_ws = phi_ws *24
 
     return(phi_ws)
 
@@ -285,25 +300,26 @@ def calculate_phi_e(T_wk,day_argue):
 
     W_2 = wind_speed * 3.6
     
-    T_ak = observedtemp[(observedtemp['day']== day_argue)]
-    T_ac = float(T_ak['max_air_temp_khu']) 
-    T_ak = T_ac +273.15
 
+    T_ak = float(daily_data['air_max']) +273.15
+
+    
 # e_s, saturated vapor pressure needs to be in T_wc deg celcius
 
-    #T_wc = T_wk - 273.15
     
     e_s = 25.374 * math.exp(17.62 - 5271/T_wk)
     
-    RH = relative_humidity[(relative_humidity['day']== day_argue)]
+    RH = float(daily_data['RH2M']) 
 
-    RH = float(RH['RH2M'])
 
 # e_a, water vapor pressure above the pond surface; unit mmHg
     
     e_a = RH * 25.374 * math.exp(17.62 - 5271/T_ak)     
 
     phi_e = float(N* W_2 * (e_s- e_a))
+    
+    phi_e = phi_e *24
+    
     return(phi_e)
 
 #create function for phi_c sensible heat transfer
@@ -314,12 +330,13 @@ def calculate_phi_c(T_wk, day_argue):
 
     W = float(wind_speed) #m/s per C&B paper
     
-    T_ak = observedtemp[(observedtemp['day']== day_argue)]
-    T_ac = float(T_ak['max_air_temp_khu']) 
+    T_ac = float(daily_data['air_max'])
     
     T_wc = T_wk - 273.15 #convert to deg celcius
     
     phi_c = float(1.5701 * W * (T_wc-T_ac))
+    
+    phi_c = phi_c *24
 
     return(phi_c)
 
@@ -353,7 +370,7 @@ def main_simulation_loop():
         print(f'phi_c: {phi_c}')
         
         phi_net = phi_sn + phi_at - phi_ws - phi_e - phi_c 
-
+        
         
 
         print(f'iteration: {count}, phi_net: {phi_net}')
@@ -365,31 +382,31 @@ def main_simulation_loop():
         #check if K or C
         print(f'iteration: {count}, H_t_1: {H_t_1}')
 
-        H_t = H_t_1 + (phi_net * area * t)
+        H_t = H_t_1 + (phi_net * area *t )
         T_w = H_t/ (volume * water_heat_capacity * water_density)
         print(f'iteration: {count}, T_w: {T_w}')
 
         #add T_w to a list somehow
-        T_wC_vecmax.append(T_w)
+        T_wC_vecmin.append(T_w)
 
         T_wk = T_w + 273.15 #convert back to kelvin
         print(T_wk)
 
 
 
-    print(T_wC_vecmax)
+    print(T_wC_vecmin)
     
-    T_wC = np.array(T_wC_vecmax)
+    T_wC = np.array(T_wC_vecmin)
 
     #df = pd.DataFrame(T_wC)
     
-    #df1= pd.concat([air_temp_data, df], axis = 1)
+    #df1= pd.concat([data, df], axis = 1)
     
     #df1.to_csv('Water_temp_daily.csv',index=False)
 
     plt.plot(T_wC, label = 'Simulated Water temp')
-    plt.plot(observedtemp['max_air_temp_khu'], label = 'Observed Maximum Air temp')
-    plt.plot(observedtemp['afternoon_water_temp_khu'], label = 'Observed Maximum Water temp')
+    plt.plot(observedtemp['min_air_temp_khu'], label = 'Observed Minimum Air temp')
+    plt.plot(observedtemp['morning_water_temp_khu'], label = 'Observed Minimum Water temp')
     plt.gca().legend(loc='center left', bbox_to_anchor=(1, 0.5))
     plt.show()
 
@@ -397,4 +414,3 @@ def main_simulation_loop():
 
 if __name__ == '__main__':
     main_simulation_loop()
-    
